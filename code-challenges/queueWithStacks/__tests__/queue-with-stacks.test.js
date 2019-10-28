6e2c37a812f78006c007088e93f7ade01c72e59d
Julie Erlemeier <jmerlemeier@gmail.com>
2019-10-28 15:48:56 -0700

    Completed stacks and queue challenge and passes 2/2 tests.

---
+++ code-challenges/queueWithStacks/__tests__/queue-with-stacks.test.js
@@ -0,0 +1,25 @@
+let Stack = require('../queue-with-stacks').stack;
+let Queue = require('../queue-with-stacks').pseudoClass;
+
+
+
+describe('Test Challenge 1', () => {
+  test('Can add a value to the stack', () => {
+    //make a new instance
+    let newElement = new Queue();
+    newElement.enqueue(8);
+    expect(newElement.stack1.storage).toEqual([8]);
+  });
+});
+
+describe('Test Challenge 2', () => {
+  test('Can remove a value from the stack', () => {
+    //make a new instance
+    let newElement = new Queue();
+    newElement.enqueue(3);
+    newElement.enqueue(4);
+    newElement.dequeue();
+    expect(newElement.stack1.storage).toEqual([]);
+    expect(newElement.stack2.storage).toEqual([4]);
+  });
+});
---
+++ code-challenges/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class Stack {
+  constructor() {
+    this.storage = [];
+  }
+
+  push(value){
+   return this.storage.push(value);
+  }
+
+  pop(){
+    return this.storage.pop();
+  }
+}
+
+class PseudoClass {
+  constructor(){
+    this.stack1 = new Stack();
+    this.stack2 = new Stack(); 
+  }
+  enqueue(value) {
+    this.stack1.push(value);
+  }
+
+  dequeue() {
+    if (this.stack2.storage.length === 0) {
+      if (this.stack1.storage.length === 0) { 
+        return 'Cannot dequeue because storage is empty'; 
+      }
+      while (this.stack1.storage.length > 0) {
+        let plate = this.stack1.storage.pop();
+        this.stack2.storage.push(plate);
+      }
+    }
+    return this.stack2.storage.pop();
+  }
+}
+
+let newQueue = new PseudoClass();
+
+// newQueue.enqueue('a');
+// console.log(newQueue.stack1);
+// newQueue.enqueue('b');
+// console.log(newQueue.stack1);
+// newQueue.enqueue('c');
+// console.log(newQueue.stack1);
+// newQueue.dequeue(); 
+// console.log(newQueue.stack1);
+
+module.exports = { 
+  stack: Stack, 
+  pseudoClass: PseudoClass, 
+}
