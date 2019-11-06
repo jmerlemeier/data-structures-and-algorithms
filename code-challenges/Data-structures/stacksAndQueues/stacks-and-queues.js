95b09e4d51daa7676675afc5a360a6ae370d455f
Julie Erlemeier <jmerlemeier@gmail.com>
2019-11-06 14:17:12 -0800

    Created Stack class and Queue class.

---
+++ code-challenges/Data-structures/stacksAndQueues/stacks-and-queues.js
@@ -0,0 +1,49 @@
+'use strict';
+
+class Node {
+  constructor(){
+    this.value = value;
+    this.next = null;
+  }
+}
+class Stack {
+  constructor() {
+    this.storage = [];
+  }
+
+  push(value){
+   return this.storage.push(value);
+  }
+
+  pop(){
+    return this.storage.pop();
+  }
+
+  peek(){
+
+  }
+
+  isEmpty(){
+
+  }
+}
+
+class Queue {
+  constructor(){
+
+  }
+
+  enqueue(){
+
+  }
+  dequeue(){
+    
+  }
+  peek(){
+
+  }
+
+  isEmpty(){
+
+  }
+}
