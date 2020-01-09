f47944992c7f49e182c682ffba4e7e8159835cf3
Julie Erlemeier <jmerlemeier@gmail.com>
2020-01-09 15:22:32 -0800

    8/8 tests complete for mulit-bracket.

--- code-challenges/Data-structures/multiBracketValidation/__tests__/multi-bracket-validation.test.js
+++ code-challenges/Data-structures/multiBracketValidation/__tests__/multi-bracket-validation.test.js
@@ -8,11 +8,33 @@ describe('Test Challenge 1', () => {
     //code here
     expect(validateBrackets('{}{Code}[Fellows](())')).toBe(true);
   });
+  test('Will return \'true\' - the brackets are balanced.', () => {
+    expect(validateBrackets('()')).toBe(true);
+  })
+  test('Will return \'true\' - the brackets are balanced.', () => {
+    expect(validateBrackets('{}(){}')).toBe(true);
+  })
+  test('Will return \'true\' - the brackets are balanced.', () => {
+    expect(validateBrackets('(){}[[]]')).toBe(true);
+  })
 });
 
 describe('Test Challenge 2', () => {
   test('Will return \'false\'  representing the brackets in the string are NOT balanced.', () => {
     //code here
+    expect(validateBrackets('{(()})')).toBe(false);
+  });
+  test('Will return \'false\'  - brackets are NOT balanced.', () => {
+    //code here
+    expect(validateBrackets('[({}]')).toBe(false);
+  });
+  test('Will return \'false\'  - brackets are NOT balanced.', () => {
+    //code here
+    expect(validateBrackets('(](')).toBe(false);
+  });
+  test('Will return \'false\'  - brackets are NOT balanced.', () => {
+    //code here
     expect(validateBrackets('{(})')).toBe(false);
   });
 });
+
